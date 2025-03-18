1beb22bdb77a722707bc0b349f6fe4f92ee4fcbb
Abhimanyu200 <abhimanyut120@gmail.com>
2025-03-18 22:46:48 +0530

    Initial commit with Firebase project

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database"; 
-import { GoogleAuthProvider,getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCxklo4ehV_I5NwZWRkDReg46I-3Rpo5A",
-  authDomain: "clone-6dd4f.firebaseapp.com",
-  projectId: "clone-6dd4f",
-  storageBucket: "clone-6dd4f.appspot.com",
-  messagingSenderId: "781876226205",
-  appId: "1:781876226205:web:0d4463c344d70c0994cc74",
-  measurementId: "G-ZT3MWT0RX8"
+  apiKey: "AIzaSyBMaRoZBsajKhPh1jH996_VQet7pm9szwg",
+  authDomain: "clone-3f9c4.firebaseapp.com",
+  projectId: "clone-3f9c4",
+  storageBucket: "clone-3f9c4.firebasestorage.app",
+  messagingSenderId: "289282326457",
+  appId: "1:289282326457:web:e285d13b33f2456dd70068",
+  measurementId: "G-9X49J813ZL"
 };
 
 // Initialize Firebase
@@ -20,3 +20,5 @@ const analytics = getAnalytics(app);
 export const auth = getAuth();
 export const db = getFirestore(app);
 export const provider = new GoogleAuthProvider();
+
+
